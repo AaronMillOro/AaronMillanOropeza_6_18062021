930cf32af0873ea33a2fa93c3d74e298b71cd10b
AaronMillOro <aaron.ibt@gmail.com>
2021-06-25 01:46:46 +0200

    :hammer: :sparkles: route and controller to get all the sauces created in the DB

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,5 +1,6 @@
 const Sauce = require('../models/Sauce');
 
+// POST new sauce 
 exports.createSauce = (req, res, next) => {
   // transforms the chain of characters JSON from the request into an object
   const sauceObject = JSON.parse(req.body.sauce);
@@ -9,5 +10,12 @@ exports.createSauce = (req, res, next) => {
   });
   sauce.save()
     .then(res.status(201).json({ message: "New sauce created"}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+// GET all sauces
+exports.getAllSauces = (req, res, next) => {
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
 };
--- routes/sauce.js
+++ routes/sauce.js
@@ -4,16 +4,9 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer_config');
 const sauceCtrl = require('../controllers/sauce');
-// to remove
-const Sauce = require('../models/Sauce');
 
 
-router.get('/', auth, (req, res, next) => {
-  Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
-
+router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 
 module.exports = router; 
